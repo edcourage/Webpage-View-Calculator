77153e9f3d5a57a103d6cb7955da0a086e9d2d35
Ed Courage <edcourage@live.com>
2020-02-23 16:57:04 +0000

    added let methods so I not calling the display double on every test, also added extra test for by most unique

--- spec/unit_tests/webpage_view_cal_spec.rb
+++ spec/unit_tests/webpage_view_cal_spec.rb
@@ -2,26 +2,25 @@ require "webpage_view_cal"
 
 describe WebpageViewCal do
 
+  let(:display) { double 'display' }
+
   context "#by_most_views" do
     # This method does not return anything. It call a display class method with the correct argument.
     # My tests will reflect that
 
     it "it parses a log file and creates a hash" do
-      display = double 'display'
       expect(display).to receive(:render_list).with({ webpage_by_most_views: { "/help_page/1" => 1 } })
       WebpageViewCal.by_most_views(file_path: 'resources/test_resources/help_page_one_view.log',
       display: display)
     end
 
     it "if the log file indecates two view that will be represented" do
-      display = double 'display'
       expect(display).to receive(:render_list).with({ webpage_by_most_views: { "/help_page/1" => 2 } })
       WebpageViewCal.by_most_views(file_path: 'resources/test_resources/help_page_two_views.log',
       display: display)
     end
 
     it "if the log file indecates different pages with different amounts of view each that will be represented" do
-      display = double 'display'
       expected_hash = {
         "/about/2" => 1,
         "/help_page/1" => 3,
@@ -37,9 +36,20 @@ describe WebpageViewCal do
     # Again this method does not return anything. It call a display class method with the correct argument.
     # My tests will reflect that
     it "should only count unique views" do
-      display = double 'display'
       expect(display).to receive(:render_list).with({ webpage_by_most_unique_views: { "/help_page/1" => 1 } })
       WebpageViewCal.by_most_unique_views(file_path: 'resources/test_resources/help_page_two_views.log',
+      display: display)
+    end
+
+    # THIS TEST IS KIND OF USELESS......
+    it "should still return unique values if it get multiple pages and views" do
+      expected_hash = {
+        "/about/2" => 1,
+        "/help_page/1" => 2,
+        "/contact" => 1,
+      }
+      expect(display).to receive(:render_list).with({ webpage_by_most_unique_views: expected_hash })
+      WebpageViewCal.by_most_unique_views(file_path: 'resources/test_resources/different_pages_multiple_views_each.log',
       display: display)
     end
   end
