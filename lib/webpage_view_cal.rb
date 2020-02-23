52ddc44c668c62ea9a603370773a6e50d2d3d27a
Ed Courage <edcourage@live.com>
2020-02-23 16:18:51 +0000

    refactored by most views method, the values are now arrays containing all the ip_address that correlate with the key

--- lib/webpage_view_cal.rb
+++ lib/webpage_view_cal.rb
@@ -1,14 +1,13 @@
 require_relative "display"
 
 class WebpageViewCal
-
   def self.by_most_views(file_path:, display: Display)
-    calculated_most_views = Hash.new(0)
+    calculated_most_views = Hash.new { |hash, key| hash[key] = [] }
     File.open(file_path).each_line do |line|
       page_name = line.split(" ")[0]
-      calculated_most_views[page_name] += 1
+      ip_address = line.split(" ")[1]
+      calculated_most_views[page_name] << ip_address
     end
-    display.render_list({ webpage_by_most_views: calculated_most_views })
+    display.render_list({ webpage_by_most_views: calculated_most_views.transform_values(&:count) })
   end
-
 end
