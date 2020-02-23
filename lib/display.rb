bfb4a6903b8ee5b39fb17f5a1562ac60870c661a
Ed Courage <edcourage@live.com>
2020-02-23 17:34:09 +0000

    unique view intagrated test added

--- lib/display.rb
+++ lib/display.rb
@@ -1,16 +1,21 @@
 class Display
   def self.render_list(hash_for_rendering)
     hash_for_rendering.each do |list_type, pages_and_view_count|
-      if list_type == :webpage_by_most_views
-        puts "** Webpages By Most Viewed **"
-      elsif list_type == :webpage_by_most_unique_views
-        puts "** Webpages By Most Unique Views **"
-      end
-
+    puts list_title_selector(list_type)
       pages_and_view_count.sort_by { |_, v| -v }.each do |page_name, view_count|
         puts "#{page_name} || Views: #{view_count}"
       end
     end
     nil
   end
+
+  def self.list_title_selector(list_type)
+    if list_type == :webpage_by_most_views
+      "** Webpages By Most Viewed **"
+    elsif list_type == :webpage_by_most_unique_views
+      "** Webpages By Most Unique Views **"
+    end
+  end
+
+  private_class_method :list_title_selector
 end
--- spec/intagrated_tests/webpage_view_cal_inter_spec.rb
+++ spec/intagrated_tests/webpage_view_cal_inter_spec.rb
@@ -8,4 +8,12 @@ describe "Intagrated Tests" do
         "** Webpages By Most Viewed **\n/help_page/1 || Views: 3\n/contact || Views: 2\n/about/2 || Views: 1\n").to_stdout
     end
   end
+
+  context "most unique views" do
+    it "will puts correct list with correct title" do
+      path = 'resources/test_resources/different_pages_multiple_views_each.log'
+      expect { WebpageViewCal.by_most_unique_views(file_path: path) }.to output(
+        "** Webpages By Most Unique Views **\n/help_page/1 || Views: 2\n/contact || Views: 1\n/about/2 || Views: 1\n").to_stdout
+    end
+  end
 end
