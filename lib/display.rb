c9e0c237669219095aa4f7f50fe44ce0008a72f7
Ed Courage <edcourage@live.com>
2020-02-23 14:25:21 +0000

    display now renders list mulitiple pages in order by view count

--- lib/display.rb
+++ lib/display.rb
@@ -1,8 +1,10 @@
 class Display
   def self.render_list(hash_for_rendering)
-
+    hash_for_rendering.each do |list_type, pages_and_view_count|
       puts "** Webpages By Most Viewed **"
-      puts "/help_page/1 || Views: 2"
-    
+      pages_and_view_count.sort_by {|k, v| -v}.each do |page_name, view_count|
+        puts "#{page_name} || Views: #{view_count}"
+      end
+    end
   end
 end
--- spec/unit_tests/display_spec.rb
+++ spec/unit_tests/display_spec.rb
@@ -6,5 +6,11 @@ describe Display do
       expect { Display.render_list({ webpage_by_most_views: { "/help_page/1" => 2 } }) }.to output(
         "** Webpages By Most Viewed **\n/help_page/1 || Views: 2\n").to_stdout
     end
+
+    it "it will return list multiple items in order of view count" do
+      expect { Display.render_list({ webpage_by_most_views: { "/help_page/1" => 2, "/contact" => 4, "/about/2" => 1 } }) }.to output(
+        "** Webpages By Most Viewed **\n/contact || Views: 4\n/help_page/1 || Views: 2\n/about/2 || Views: 1\n").to_stdout
+    end
+    
   end
 end
