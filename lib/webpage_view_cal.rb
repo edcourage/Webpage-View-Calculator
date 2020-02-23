de48ee16d01cd26353bab00728e015b710b0a8a4
Ed Courage <edcourage@live.com>
2020-02-23 16:46:55 +0000

    created the by most unique views method... passed first test I have a good feeling it will also pass the next test. I have also created a new private class method that does the log parsing in the aim of keeping my code DRY

--- lib/webpage_view_cal.rb
+++ lib/webpage_view_cal.rb
@@ -2,12 +2,27 @@ require_relative "display"
 
 class WebpageViewCal
   def self.by_most_views(file_path:, display: Display)
+    display.render_list({
+      webpage_by_most_views: log_parser(file_path).transform_values { |v| v.count }
+      })
+  end
+
+  def self.by_most_unique_views(file_path:, display: Display)
+    display.render_list({
+      webpage_by_most_unique_views: log_parser(file_path).transform_values { |v| v.uniq.count }
+      })
+  end
+
+  # This method returns a hash where the keys equal the page and and the value is the correlating ip addresses
+  def self.log_parser(file_path)
     calculated_most_views = Hash.new { |hash, key| hash[key] = [] }
     File.open(file_path).each_line do |line|
       page_name = line.split(" ")[0]
       ip_address = line.split(" ")[1]
       calculated_most_views[page_name] << ip_address
     end
-    display.render_list({ webpage_by_most_views: calculated_most_views.transform_values(&:count) })
+    calculated_most_views
   end
+
+  private_class_method :log_parser
 end
--- spec/unit_tests/webpage_view_cal_spec.rb
+++ spec/unit_tests/webpage_view_cal_spec.rb
@@ -3,7 +3,7 @@ require "webpage_view_cal"
 describe WebpageViewCal do
 
   context "#by_most_views" do
-    # by default this method does not return anything. It call a display class method with the correct argument.
+    # This method does not return anything. It call a display class method with the correct argument.
     # My tests will reflect that
 
     it "it parses a log file and creates a hash" do
@@ -29,6 +29,17 @@ describe WebpageViewCal do
       }
       expect(display).to receive(:render_list).with({ webpage_by_most_views: expected_hash })
       WebpageViewCal.by_most_views(file_path: 'resources/test_resources/different_pages_multiple_views_each.log',
+      display: display)
+    end
+  end
+
+  context "#by_most_unique_views" do
+    # Again this method does not return anything. It call a display class method with the correct argument.
+    # My tests will reflect that
+    it "should only count unique views" do
+      display = double 'display'
+      expect(display).to receive(:render_list).with({ webpage_by_most_unique_views: { "/help_page/1" => 1 } })
+      WebpageViewCal.by_most_unique_views(file_path: 'resources/test_resources/help_page_two_views.log',
       display: display)
     end
   end
