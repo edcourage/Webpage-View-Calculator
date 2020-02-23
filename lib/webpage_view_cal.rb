d0f518e008b9267f020e7d11eefe9dfbfbcfb96d
Ed Courage <edcourage@live.com>
2020-02-23 13:30:59 +0000

    yes I was correct the last commit inadvertenly completed the metheds functionality

--- lib/webpage_view_cal.rb
+++ lib/webpage_view_cal.rb
@@ -2,11 +2,11 @@ class WebpageViewCal
 
   def self.by_most_views(file_path:, display: Display)
     calculated_most_views = Hash.new(0)
-    wed_server_log = File.open(file_path)
-    wed_server_log.each_line do |line|
-      line = line.split(" ")
-      calculated_most_views[line[0]] += 1
+    File.open(file_path).each_line do |line|
+      page_name = line.split(" ")[0]
+      calculated_most_views[page_name] += 1
     end
+    p calculated_most_views
     display.render_list({ webpage_by_most_views: calculated_most_views })
   end
 
--- spec/unit_tests/webpage_view_cal_spec.rb
+++ spec/unit_tests/webpage_view_cal_spec.rb
@@ -19,6 +19,18 @@ describe WebpageViewCal do
       WebpageViewCal.by_most_views(file_path: 'resources/test_resources/help_page_two_views.log',
       display: display)
     end
+
+    it "if the log file indecates different pages with different amounts of view each that will be represented" do
+      display = double 'display'
+      expected_hash = {
+        "/about/2" => 1,
+        "/help_page/1" => 3,
+        "/contact" => 2,
+      }
+      expect(display).to receive(:render_list).with({ webpage_by_most_views: expected_hash })
+      WebpageViewCal.by_most_views(file_path: 'resources/test_resources/different_pages_multiple_views_each.log',
+      display: display)
+    end
   end
 
 end
